9656e5f52f32b3c8714cbd3a1c2f6d982342a1ab
yuemingw233 <44715026+yuemingw233@users.noreply.github.com>
2019-05-20 02:44:09 -0700

    Jumping

--- HeartBeat/Assets/Script/PlayerScript.cs
+++ HeartBeat/Assets/Script/PlayerScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+    bool isJumping;
+
+    private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float move = 1;
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+        Jump();
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            rb.velocity = Vector2.zero;
+        }
     }
 }
